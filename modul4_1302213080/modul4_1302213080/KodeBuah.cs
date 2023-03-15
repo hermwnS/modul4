7620258d167d51132c511e24132a89092e04ec8a
hermwnS <126874286+hermwnS@users.noreply.github.com>
2023-03-15 17:14:08 +0700

    Add files via upload

---
+++ modul4_1302213080/modul4_1302213080/KodeBuah.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class KodeBuah
+{
+        public enum Buah
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        };
+
+    public static string getKodeBuah(Buah namaBuah)
+    {
+    string[] kode = {"A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00",
+            "L00", "M00", "N00", "O00"};
+    return kode[(int)namaBuah];
+    }
+}
---
+++ modul4_1302213080/modul4_1302213080/Program.cs
@@ -0,0 +1,31 @@
+﻿using KodeBuah.cs;
+public class Program
+{
+    enum State { Jongkok, Tengkurap, Berdiri, Terbang };
+    internal class Program
+    {
+        public class PosisiKarakterGame
+        {
+            State state = State.Berdiri;
+            switch (State.Terbang)
+            {
+                case State.Jongkok
+                {
+                    Console.WriteLine("posisi landing");
+                }
+    }
+           switch (State.Berdiri)
+           {
+                case State.Terbang
+                {
+                    Console.WriteLine("posisi take off");
+                }
+           }
+        }
+        private static void Main(string[] args)
+        {
+            KodeBuah.getKodeBuah(namaBuah);
+            
+        }
+    }   
+}
